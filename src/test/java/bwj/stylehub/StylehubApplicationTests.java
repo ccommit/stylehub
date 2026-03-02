ffec18101e86c8543dfe7fe8d03efad47cb44548
WonJin <try3982@kakao.com>
2026-03-02 16:08:37 +0900

    -. 프로젝트 생성

---
+++ src/test/java/bwj/stylehub/StylehubApplicationTests.java
@@ -0,0 +1,13 @@
+package bwj.stylehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
