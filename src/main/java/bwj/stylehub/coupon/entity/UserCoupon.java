a0e32da766e6f4dcc4076b8f1110be10d9f86d6d
WonJin <try3982@kakao.com>
2026-03-14 19:00:14 +0900

    refactor: 모든 엔티티 클래스의 JPA 와일드카드 import를 명시적 import로 교체

--- src/main/java/bwj/stylehub/coupon/entity/UserCoupon.java
+++ src/main/java/bwj/stylehub/coupon/entity/UserCoupon.java
@@ -2,7 +2,18 @@ package bwj.stylehub.coupon.entity;
 
 import bwj.stylehub.coupon.enums.CouponStatus;
 import bwj.stylehub.user.entity.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
