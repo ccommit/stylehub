1f3afd1319b7a04d2762f525fe90b14f09b26da1
WonJin <try3982@kakao.com>
2026-03-21 08:15:02 +0900

    refactor: orderstatus.  PREPARING,  // 배송 준비 중  삭제

--- src/main/java/bwj/stylehub/order/enums/OrderStatus.java
+++ src/main/java/bwj/stylehub/order/enums/OrderStatus.java
@@ -7,7 +7,6 @@ public enum OrderStatus {
 
     PENDING,    // 주문 완료 (결제 대기)
     PAID,       // 결제 완료
-    PREPARING,  // 배송 준비 중
     SHIPPING,   // 배송 중
     DELIVERED,  // 배송 완료
     CANCELLED   // 주문 취소
