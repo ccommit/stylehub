3c406a866b1a952e1901aa2fe74cac1f70d59a14
WonJin <try3982@kakao.com>
2026-03-20 10:33:06 +0900

    refactor:EnableJpaAuditing 역할 주석 추가
    -.  runtimeOnly 'com.mysql:mysql-connector-j'가 필요한 이유 주석 추가

--- src/main/java/bwj/stylehub/StylehubApplication.java
+++ src/main/java/bwj/stylehub/StylehubApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableJpaAuditing // @CreatedDate, @LastModifiedDate 자동 설정 활성화
 @SpringBootApplication
 public class StylehubApplication {
 
