e027dc1164d2dbbdeff24b9315dc6847e08ab245
WonJin <try3982@kakao.com>
2026-03-24 20:45:26 +0900

    refactor: javadocs 주석 추가(수정일자)

--- src/main/java/ccommit/stylehub/common/exception/ErrorResponse.java
+++ src/main/java/ccommit/stylehub/common/exception/ErrorResponse.java
@@ -3,6 +3,16 @@ package ccommit.stylehub.common.exception;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+/**
+ * @author WonJin Bae
+ * @created 2026/03/22
+ * @modified 2026/03/24 by WonJin - refactor: bwj 패키지명 ccommit으로 변경, timestamp/path/traceId 필드 추가
+ *
+ * <p>
+ * 클라이언트에 반환되는 에러 응답 포맷을 정의한다.
+ * timestamp, path, traceId로 장애 추적 및 모니터링을 지원한다.
+ * </p>
+ */
 public record ErrorResponse(
         int status,
         String code,
