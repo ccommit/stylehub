98abac7636f799d01b9a2db1dfd60fde42405b62
WonJin <try3982@kakao.com>
2026-03-26 00:05:37 +0900

    refactor:  Arrays.asList() 제거

--- src/main/java/ccommit/stylehub/common/config/RoleCheckInterceptor.java
+++ src/main/java/ccommit/stylehub/common/config/RoleCheckInterceptor.java
@@ -11,14 +11,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.util.Arrays;
-
 /**
  * @author WonJin Bae
  * @created 2026/03/23
+ * @modified 2026/03/26 by WonJin - refactor: 불필요한 List 객체 생성 제거
  *
  * <p>
- * 컨트롤러 메서드에 @RequiredRole이 선언된 경우 세션의 역할 정보를 검증하는 인터셉터이다.
+ * 컨트롤러 클래스 또는 메서드에 @RequiredRole이 선언된 경우 세션의 역할 정보를 검증하는 인터셉터이다.
+ * 메서드 레벨이 클래스 레벨보다 우선 적용된다.
  * 요구 역할과 일치하지 않으면 FORBIDDEN 예외를 던진다.
  * </p>
  */
@@ -46,11 +46,19 @@ public class RoleCheckInterceptor implements HandlerInterceptor {
             throw new BusinessException(ErrorCode.UNAUTHORIZED);
         }
 
-        boolean hasRole = Arrays.asList(requiredRole.value()).contains(userRole);
-        if (!hasRole) {
+        if (!hasRequiredRole(requiredRole.value(), userRole)) {
             throw new BusinessException(ErrorCode.FORBIDDEN);
         }
 
         return true;
+    }
+
+    private boolean hasRequiredRole(UserRole[] requiredRoles, UserRole userRole) {
+        for (UserRole role : requiredRoles) {
+            if (role == userRole) {
+                return true;
+            }
+        }
+        return false;
     }
 }
